b5bcd536e483c88b8c3faef9f59dd3b0477ccd97
shamiko <up202108672@up.pt>
2023-05-08 20:52:51 +0100

    Login and register are working now

--- src/action_register.php
+++ src/action_register.php
@@ -15,11 +15,18 @@ require_once(__DIR__. '/database/user.class.php');
 
 $password_regex = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$";
 
-if(preg_match($password_regex, $_POST['rpass']) == 0){
+$flag = 1;
+
+if($_POST['rpass'] != $_POST['rpassrepeat']){
+    $flag = 0;
+}
+
+if((preg_match($password_regex, $_POST['rpass']) == 0) and $flag == 1){
     $user = User::addUser($db, $_POST['rmail'], $_POST['rname'], $_POST['rpass']);
 
     $session->setID($user->id);
     $session->setName($user->username);
+    $session->addMessage('sucess', 'register successfull');
 
     header('Location: ' . index.php);
 } else {

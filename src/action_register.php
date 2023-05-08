3b3d3b8ffb4630ad31491b5189309f01a12536d3
shamiko <up202108672@up.pt>
2023-05-08 20:44:28 +0100

    Removed conditions on register for now

--- src/action_register.php
+++ src/action_register.php
@@ -13,47 +13,15 @@ $db = getDatabaseConnection();
 
 require_once(__DIR__. '/database/user.class.php');
 
-$flag = 1;
+$password_regex = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$";
 
-$lenght_regex = "^{8,}$";
-$number_regex = "^(?=.*\d)$";
-$upper_case_regex = "^(?=.*[A-Z])$";
-$lower_case_regex = "^(?=.*[a-z])$";
-
-if($_POST['rpass'] != $_POST['rpassrepeat']){
-    $session->addMessage('failure', 'Not enough characters');
-    $flag = 0;
-    header('Location:' . $_SERVER['HTTP_REFERER']);
-}
-
-
-if(preg_match($lenght_regex, $_POST['rpass']) == 0){
-    $session->addMessage('failure', 'Not enough characters');
-    $flag = 0;
-    header('Location:' . $_SERVER['HTTP_REFERER']);
-}
-
-if(preg_match($number_regex, $_POST['rpass']) == 0){
-    $session->addMessage('failure', 'At least one digit in the password');
-    $flag = 0;
-    header('Location:' . $_SERVER['HTTP_REFERER']);
-}
-
-if(preg_match($upper_case_regex, $_POST['rpass']) == 0){
-    $session->addMessage('failure', 'At least one upper case character in the password');
-    $flag = 0;
-    header('Location:' . $_SERVER['HTTP_REFERER']);
-}
-
-
-if((preg_match($lower_case_regex, $_POST['rpass']) == 0) and $flag == 1){
+if(preg_match($password_regex, $_POST['rpass']) == 0){
     $user = User::addUser($db, $_POST['rmail'], $_POST['rname'], $_POST['rpass']);
 
     $session->setID($user->id);
     $session->setName($user->username);
-    $session->addMessage('sucess', 'register successful');
 
     header('Location: ' . index.php);
 } else {
-    header('Location:' . $_SERVER['HTTP_REFERER']);
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
 }

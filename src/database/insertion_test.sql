5e5e15fda7c352717469ba6fe6f2e1f8bdbab720
Shamiko <gpnpinho@gmail.com>
2023-05-11 15:04:49 +0100

    Changed some user parameters for profile later on

--- src/database/insertion_test.sql
+++ src/database/insertion_test.sql
@@ -3,4 +3,4 @@ INSERT INTO Admin VALUES (1);
 INSERT INTO Status(adminID,name) VALUES (1, "open");
 INSERT INTO Department(adminID, name) VALUES (1, "Anime");
 INSERT INTO Task(agentID, content) VALUES (1, "Hatsune Miku");
-INSERT INTO Ticket(clientID, departmentID, taskID, status_name, title, priority, da) VALUES (1,1,1, "open", "Hatsune Miku",1, 2222-22-22);
+INSERT INTO Ticket(clientID, department, taskID, status_name, title, priority, da) VALUES (1,"Anime",1, "open", "Hatsune Miku",1, 2222-22-22);
--- src/database/user.class.php
+++ src/database/user.class.php
@@ -2,24 +2,30 @@
 
 class User {
     public int $id;
+    public string $firstname;
+    public string $lastname;
     public string $username;
     public string $email;
 
-public function __construct(int $id, string $username, string $email){
+public function __construct(int $id, string $firstname, string $lastname, string $username, string $email){
       $this->id = $id;
+      $this->firstname = $firstname;
+      $this->lastname = $lastname;
       $this->username = $username;
       $this->email = $email;
     }
 
 
     static function getUser(PDO $db, string $username, string $password) : ?User {
-      $stmt = $db->prepare('SELECT userID, username, email
+      $stmt = $db->prepare('SELECT userID, firstname, lastname, username, email
         FROM User
         WHERE lower(username) = ? AND password = ?');
       $stmt->execute(array(strtolower($username), sha1($password)));
       if($user = $stmt->fetch()){
         return new User(
           $user['userID'],
+          $user['firstname'],
+          $user['lastname'],
           $user['username'],
           $user['email']
         );
@@ -28,7 +34,7 @@ public function __construct(int $id, string $username, string $email){
 
 
     static function addUser(PDO $db, string $email, string $username, string $password) : ?User{
-      $stmt = $db->prepare('INSERT INTO User (username, password, email) VALUES (?, ?, ?)');
+      $stmt = $db->prepare('INSERT INTO User (username,firstname, lastname, password, email) VALUES (?, "", "", ?, ?)');
       $stmt->execute(array(strtolower($username), sha1($password), strtolower($email)));
       return User::getUser($db, $username, $password);
     }

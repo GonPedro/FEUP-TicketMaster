f431a831cc4e788ff14b0daafa83c5d77d27ed91
Shamiko <gpnpinho@gmail.com>
2023-05-22 00:34:45 +0100

    Messages were not drawing for author

--- src/ticket.php
+++ src/ticket.php
@@ -28,7 +28,7 @@ if($session->isLoggedin()){
     drawTicketInfo($ticket);
     $name = User::getName($db, $session->getID());
     $role = User::getRole($db, $session->getID());
-    if((Ticket::checkAssignedAgent($db, (int)$_GET['id'], $name)) or ($role == "admin")){
+    if((Ticket::checkAssignedAgent($db, (int)$_GET['id'], $name)) or ($role == "admin") or ($name == $ticket->client_name)){
         drawMessageInput();
         drawMessages($session, $messages);
     }
